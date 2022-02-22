d9b781d23b634afae60d79e87aa96747d88ba5db
Thomaz <tjhonnar@gmail.com>
2022-02-22 18:52:43 -0500

    app vs1.1

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+//Exportar configuraciones
+module.exports = {
+    mongodb: {
+        URI: `mongodb+srv://Tjhonnar:Rodrisebas78#@junior0.fwul8.mongodb.net/login?retryWrites=true&w=majority`,
+    }
+};
